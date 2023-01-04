14c9d91e9a725b6045543cdb0014a4713e8f5674
VinayTShetty <vinaytshetty@gmail.com>
2023-01-04 11:57:25 +0530

    Fragment Data Transimission from One Fragment to Another Fragment

--- app/src/main/java/com/example/project/DataDetailsFragment.kt
+++ app/src/main/java/com/example/project/DataDetailsFragment.kt
@@ -6,14 +6,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.data.Countries
 
 class DataDetailsFragment : Fragment() {
-
+    private lateinit var countryFlag: ImageView
+    private lateinit var countryDetails: TextView
     private val args by navArgs<DataDetailsFragmentArgs>()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         Log.d("Data", "onCreateView: ${args.countriesData.capital}")
+        val view=inflater.inflate(R.layout.fragment_data_details, container, false)
+        val countryObject=args.countriesData
+        intializeWidgets(view,countryObject)
+        return view
+    }
 
-        return inflater.inflate(R.layout.fragment_data_details, container, false)
+    fun intializeWidgets(view: View, countries: Countries){
+        val countryinfo=
+            "\n"+"Name "+args.countriesData.name
+        "\n"+"Capital "+args.countriesData.capital
+        "\n"+"Region "+args.countriesData.region
+        countryFlag=view.findViewById(R.id.countryFlag)
+        countryDetails=view.findViewById(R.id.countryDetails)
+        countryDetails.setText(countryinfo.toString())
+        context?.let {
+            Glide.with(it)
+                .load(countries.flags?.png)
+                .into(countryFlag)
+        }
     }
 }
